8f0618986343f6de5eed06720cb029d8c5421420
Ekaiva <Ekaiva@gmail.com>
2023-09-11 10:56:36 +0530

    first commit

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -2,58 +2,62 @@ import React, { useState } from 'react'
 import { loginApi } from '../service/authentication';
 
 const Login = () => {
-    const [formInput,setForminput]=useState("")
+    const [formInput,setForminput]=useState({
+        login_type:"email"
+    })
     const handleInput =(e)=>{
         const {name,value} = e.target;
         setForminput({...formInput,[name]:value})
     }
 
-    const handleLogin =()=>{
+    const handleLogin =(e)=>{
+        e.preventDefault()
         let data = formInput
         loginApi(data).then((res)=>{
-            console.log(res)
+            localStorage.setItem("access_token",res?.data?.access_token)
+            window.location.href="/dashboard"
         }).catch((err)=>{
-            console.log("err")
+            console.log("err",err)
             alert(err?.response?.data?.message||"Internal server error")
         })
     }
   return (
     <>
         <div className='login'>
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-2"></div>
-                    <div class="col-lg-6 col-md-8 login-box">
-                        <div class="col-lg-12 login-key">
-                            <i class="fa fa-key" aria-hidden="true"></i>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3 col-md-2"></div>
+                    <div className="col-lg-6 col-md-8 login-box">
+                        <div className="col-lg-12 login-key">
+                            <i className="fa fa-key" aria-hidden="true"></i>
                         </div>
-                        <div class="col-lg-12 login-title">
+                        <div className="col-lg-12 login-title">
                             ADMIN PANEL
                         </div>
 
-                        <div class="col-lg-12 login-form">
-                            <div class="col-lg-12 login-form">
+                        <div className="col-lg-12 login-form">
+                            <div className="col-lg-12 login-form">
                                 <form>
-                                    <div class="form-group">
-                                        <label class="form-control-label">USERNAME</label>
-                                        <input name="name" type="text" class="form-control" onChange={handleInput}/>
+                                    <div className="form-group">
+                                        <label className="form-control-label">USERNAME</label>
+                                        <input name="email" type="text" className="form-control" onChange={handleInput}/>
                                     </div>
-                                    <div class="form-group">
-                                        <label class="form-control-label">PASSWORD</label>
-                                        <input name="password" type="password" class="form-control"  onChange={handleInput} />
+                                    <div className="form-group">
+                                        <label className="form-control-label">PASSWORD</label>
+                                        <input name="password" type="password" className="form-control"  onChange={handleInput} />
                                     </div>
 
-                                    <div class="col-lg-12 loginbttm">
-                                        <div class="col-lg-6 login-btm login-text">
+                                    <div className="col-lg-12 loginbttm">
+                                        <div className="col-lg-6 login-btm login-text">
                                         </div>
-                                        <div class="col-lg-6 login-btm login-button">
-                                            <button type="submit" class="btn btn-outline-primary" onClick={handleLogin}>LOGIN</button>
+                                        <div className="col-lg-6 login-btm login-button">
+                                            <button type="submit" className="btn btn-outline-primary" onClick={handleLogin}>LOGIN</button>
                                         </div>
                                     </div>
                                 </form>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-md-2"></div>
+                        <div className="col-lg-3 col-md-2"></div>
                     </div>
                 </div>
             </div>
